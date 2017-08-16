c6cf0c7971a7140cf48d5bb28edab98bd5f72735
Shahin <shahin.belim@tops-int.com>
2017-08-16 17:26:38 +0530

    Code updated.

--- angular/app.config.js
+++ angular/app.config.js
@@ -1,7 +1,7 @@
 angular.module('myApp.config', []);
 var config_data = {
 	'GENERAL_CONFIG': {
-		'app_base_url': window.location.origin, //'http://topsdemo.in:8010',
+		'app_base_url': window.location.origin,
 		'app_image_url':'assets/images/'
 	}
 }
